ea63cf57fd158b23714241e0a0ece76d6b306854
Guda Sriram <96776077+GudaSriram@users.noreply.github.com>
2023-11-29 19:54:05 +0530

    Update MyFunction.cs

--- .github/workflows/MyFunction.cs
+++ .github/workflows/MyFunction.cs
@@ -4,6 +4,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
 
 public static class MyFunction
 {
